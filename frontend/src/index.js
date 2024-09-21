57db55c64af9d454d9abeea28999bb4777787d0d
abhisheksh99 <abhishekleo1234@gmail.com>
2024-09-21 13:35:57 -0400

    Added HomeScreen and its routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './assets/styles/index.css';
-import './assets/styles/bootstrap.custom.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/styles/index.css";
+import "./assets/styles/bootstrap.custom.css";
+import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index={true} path="/" element={<HomeScreen />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
-
-
