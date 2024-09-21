4bed924233827ac91bb79352d46119a8a39855aa
abhisheksh99 <abhishekleo1234@gmail.com>
2024-09-21 12:49:06 -0400

    Added header and footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-     App
-    </div>
-  );
+    <>
+    <Header />
+    <main className="py-3">
+      <Container>
+        <h1>Welcome to QuickDeal</h1>
+      </Container>
+
+    </main>
+    <Footer />
+    
+    </>
+  )
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './assets/styles/index.css';
+import './assets/styles/bootstrap.custom.css';
 import App from './App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
