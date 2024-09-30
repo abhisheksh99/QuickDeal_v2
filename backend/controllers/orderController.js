9530a82cfdba60cce832efd043e6c7609126a96f
abhisheksh99 <abhishekleo1234@gmail.com>
2024-09-30 10:50:44 -0400

    order controller updated

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -100,5 +100,8 @@ export const updateOrderToDelivered = asyncHandler(async (req, res) => {
 // @route   GET /api/orders/:id/pay
 // @access  Private/admin
 export const getOrders = asyncHandler(async (req, res) => {
-    res.send("get all orders")
+  const orders = await Order.find({}).populate("user","id name")
+  res.status(200).json(orders)
+
 })
+    
