6899a52734b7a5f985f79b3ca45e125b91f97b57
abhisheksh99 <abhishekleo1234@gmail.com>
2024-09-29 23:34:38 -0400

    order screen added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,11 +15,14 @@ import ShippingScreen from "./screens/ShippingScreen";
 import PrivateRoute from "./components/PrivateRoute";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
+import {PayPalScriptProvider} from "@paypal/react-paypal-js"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PayPalScriptProvider deferLoading={true}>
       <Router>
         <Routes>
           <Route path="/" element={<App />}>
@@ -33,10 +36,13 @@ root.render(
               <Route path="/shipping" element={<ShippingScreen />} />
               <Route path="/payment" element={<PaymentScreen />} />
               <Route path="/placeorder" element={<PlaceOrderScreen />} />
+              <Route path="/order/:id" element={<OrderScreen />} />
             </Route>
           </Route>
         </Routes>
       </Router>
+      </PayPalScriptProvider>
+      
     </Provider>
   </React.StrictMode>
 );
--- frontend/src/slices/orderApiSlice.js
+++ frontend/src/slices/orderApiSlice.js
@@ -1,17 +1,35 @@
 import { apiSlice } from "./apiSlice";
 
 export const orderApiSlice = apiSlice.injectEndpoints({
-    endpoints : (builder) =>({
+    endpoints: (builder) => ({
         createOrder: builder.mutation({
-            query: (order) =>({
-                url:"/api/orders",
+            query: (order) => ({
+                url: "/api/orders",
                 method: "POST",
                 body: {...order}
             })
+        }),
+        getOrderDetails: builder.query({
+            query: (id) => ({
+                url: `/api/orders/${id}`,
+                method: "GET",
+            }),
+            keepUnusedDataFor: 5
+        }),
+        payOrder: builder.mutation({
+            query: ({id, details}) => ({
+                url: `/api/orders/${id}/pay`,
+                method: "PUT",
+                body: {...details}
+            })
+        }),
+        getPayPalClientID: builder.query({
+            query: () => ({
+                url: "/api/config/paypal"
+            }),
+            keepUnusedDataFor: 5,
         })
-
     })
-})
+});
 
-
-export const {useCreateOrderMutation} = orderApiSlice 
+export const { useCreateOrderMutation, useGetOrderDetailsQuery, usePayOrderMutation, useGetPayPalClientIDQuery } = orderApiSlice;
