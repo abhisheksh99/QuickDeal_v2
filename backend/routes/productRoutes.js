2e474121a6628418c2e5107d2f2effbecb9486b9
abhisheksh99 <abhishekleo1234@gmail.com>
2024-09-30 22:41:26 -0400

    Product delete controller and route added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getAllProducts, getProductById, createProduct,updateProduct } from "../controllers/productController.js"
+import { getAllProducts, getProductById, createProduct,updateProduct,deleteProduct } from "../controllers/productController.js"
 import {protect, admin} from "../middleware/authMiddleware.js"
 
 
@@ -8,7 +8,7 @@ const router = express.Router()
 
 
 router.route("/").get(getAllProducts).post(protect,admin,createProduct)
-router.route("/:id").get(getProductById).put(protect,admin,updateProduct)
+router.route("/:id").get(getProductById).put(protect,admin,updateProduct).delete(protect,admin,deleteProduct)
 
 
 
