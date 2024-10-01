8a0f292508a1dc046f7b9741311d0bc54ca88fd4
abhisheksh99 <abhishekleo1234@gmail.com>
2024-10-01 13:03:26 -0400

    create product review backedn functionality added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getAllProducts, getProductById, createProduct,updateProduct,deleteProduct } from "../controllers/productController.js"
+import { getAllProducts, getProductById, createProduct,updateProduct,deleteProduct, createProductReview } from "../controllers/productController.js"
 import {protect, admin} from "../middleware/authMiddleware.js"
 
 
@@ -9,6 +9,7 @@ const router = express.Router()
 
 router.route("/").get(getAllProducts).post(protect,admin,createProduct)
 router.route("/:id").get(getProductById).put(protect,admin,updateProduct).delete(protect,admin,deleteProduct)
+router.route("/:id/reviews").post(protect,createProduct)
 
 
 
