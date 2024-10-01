d3f470000b7389aaf831b50f2334c87b72709eaa
abhisheksh99 <abhishekleo1234@gmail.com>
2024-09-30 21:54:46 -0400

    Update product controller and routes added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getAllProducts, getProductById, createProduct } from "../controllers/productController.js"
+import { getAllProducts, getProductById, createProduct,updateProduct } from "../controllers/productController.js"
 import {protect, admin} from "../middleware/authMiddleware.js"
 
 
@@ -8,7 +8,7 @@ const router = express.Router()
 
 
 router.route("/").get(getAllProducts).post(protect,admin,createProduct)
-router.route("/:id").get(getProductById)
+router.route("/:id").get(getProductById).put(protect,admin,updateProduct)
 
 
 
